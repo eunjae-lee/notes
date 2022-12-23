eb46b51e3b26b3f14bf8332f3575926fdc37e312
Eunjae Lee <hey@eunjae.dev>
2022-12-23 10:48:36 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,11 +2,10 @@ import { Config } from "./src/types";
 
 const config: Config = {
   labels: {
-    publish: [{ type: "twitter" }, { type: "mastodon" }],
-    // publish_ko: [
-    //   { type: "twitter", env_var_prefix: "KO_" },
-    //   { type: "mastodon", env_var_prefix: "KO_" },
-    // ],
+    publish_ko: [
+      { type: "twitter", env_var_prefix: "KO_" },
+      { type: "mastodon", env_var_prefix: "KO_" },
+    ],
   },
 };
 
